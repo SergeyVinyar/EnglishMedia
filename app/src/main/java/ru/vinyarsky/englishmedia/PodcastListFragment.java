e7ae46d833dce605d14934059eb024f636084a92
Sergey Vinyarsky <sergey@vinyarsky.ru>
2017-03-29 15:21:11 +0300

    Initial commit

---
+++ app/src/main/java/ru/vinyarsky/englishmedia/PodcastListFragment.java
@@ -0,0 +1,77 @@
+package ru.vinyarsky.englishmedia;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class PodcastListFragment extends Fragment implements ListView.OnItemClickListener {
+
+
+    private OnPodcastListFragmentListener mListener;
+
+    public PodcastListFragment() {
+        // Required empty public constructor
+    }
+
+    public static PodcastListFragment newInstance() {
+        return new PodcastListFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_podcastlist, container, false);
+
+        ListView listView = (ListView) view.findViewById(R.id.listview_fragment_podcastlist);
+        listView.setOnItemClickListener(this);
+
+        List<String> data = new ArrayList<>(20);
+        for(int i = 0; i < 20; i++)
+            data.add(Integer.toString(i));
+        listView.setAdapter(new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_list_item_1, data));
+
+        return view;
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if (mListener != null) {
+            mListener.onSelectPodcast(UUID.randomUUID());
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnPodcastListFragmentListener) {
+            mListener = (OnPodcastListFragmentListener) context;
+        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnPodcastListFragmentListener {
+        void onSelectPodcast(UUID podcastId);
+    }
+}
